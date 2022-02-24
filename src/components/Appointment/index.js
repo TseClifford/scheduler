f5383983929596e01d63b7259e98bc0307ca3f01
Clifford Tse <TseClifford@gmail.com>
2022-02-24 00:02:07 -0500

    update Appointment component with Header and Show/Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
-
   return (
     <article className="appointment">
-      Appointment is at {props.time}
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+        :
+        <Empty />}
     </article>
   );
 }
