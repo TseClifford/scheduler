7fcdb20c7eb14f0d196c5a201e4be55b2bd2ae7c
Clifford Tse <TseClifford@gmail.com>
2022-02-24 23:55:22 -0500

    retrieving appointments using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "./Appointment";
+import getAppointmentsForDay from "helpers/selectors"
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -12,57 +13,25 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-  
+  const setDay = day => setState(prev => ({ ...prev, day }));
+
   useEffect(() => {
-    axios
-      .get("/api/days")
-      .then((response) => {
-        setDays([...response.data]);
-      })
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+    ])
+      .then((all) => {
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data
+        }))
+      });
   }, []);
 
-  const appointments = [
-    {
-      id: 1,
-      time: "12pm",
-    },
-    {
-      id: 2,
-      time: "1pm",
-      interview: {
-        student: "Lydia Miller-Jones",
-        interviewer: {
-          id: 3,
-          name: "Sylvia Palmer",
-          avatar: "https://i.imgur.com/LpaY82x.png",
-        }
-      }
-    },
-    {
-      id: 3,
-      time: "2pm",
-    },
-    {
-      id: 4,
-      time: "3pm",
-      interview: {
-        student: "Archie Andrews",
-        interviewer: {
-          id: 4,
-          name: "Cohana Roy",
-          avatar: "https://i.imgur.com/FK8V841.jpg",
-        }
-      }
-    },
-    {
-      id: 5,
-      time: "4pm",
-    }
-  ];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const appList = appointments.map((app) => {
+  const appList = dailyAppointments.map((app) => {
     return (
       <Appointment key={app.id} {...app} />
     );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const findDay = state.days.find(stateDay => stateDay.name === day);
 
   return findDay ? findDay.appointments.map((app) => state.appointments[app]) : [];
