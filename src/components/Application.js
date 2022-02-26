16f7e966b07a7565b201de2b97094d0b7475e9f2
Clifford Tse <TseClifford@gmail.com>
2022-02-25 19:00:54 -0500

    update variable names, add getInterview helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,14 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "./Appointment";
-import getAppointmentsForDay from "helpers/selectors"
+import { getAppointmentsForDay, getInterview } from "helpers/selectors"
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState(prev => ({ ...prev, day }));
@@ -19,23 +20,33 @@ export default function Application(props) {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ])
       .then((all) => {
         setState(prev => ({
           ...prev,
           days: all[0].data,
-          appointments: all[1].data
+          appointments: all[1].data,
+          interviewers: all[2].data
         }))
       });
   }, []);
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
 
-  const appList = dailyAppointments.map((app) => {
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
-      <Appointment key={app.id} {...app} />
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview} />
     );
   })
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -59,7 +70,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appList}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,21 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const findDay = state.days.find(stateDay => stateDay.name === day);
 
   return findDay ? findDay.appointments.map((app) => state.appointments[app]) : [];
+}
+
+export function getInterview(state, interview) {
+  let findInterview = null;
+
+  if (interview) {
+    findInterview = { ...interview };
+  }
+
+  return findInterview ?
+    {
+      student: findInterview.student,
+      interviewer: { ...state.interviewers[findInterview.interviewer] }
+    }
+    :
+    null;
 }
